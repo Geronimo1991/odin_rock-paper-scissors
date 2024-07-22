dadc4671ef3a230425b528067b1adf2d72e99176
Bartosz Gierej <b.gierej@investa.pl>
2024-07-22 07:47:13 +0200

    Add UI to the game

--- script.js
+++ script.js
@@ -1,10 +1,72 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const result = document.querySelector(".result");
 let humanScore = 0;
 let computerScore = 0;
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+rock.addEventListener("click", () => {
+	playRound("rock", getComputerChoice());
+});
+paper.addEventListener("click", () => {
+	playRound("paper", getComputerChoice());
+});
+scissors.addEventListener("click", () => {
+	playRound("scissors", getComputerChoice());
+});
 
-function playRound(humanSelection, computerSelection) {}
+function playRound(humanSelection, computerSelection) {
+	if (humanSelection === computerSelection) {
+		result.textContent =
+			"This round is a draw!" + currentScore(humanScore, computerScore);
+	}
+
+	if (humanSelection === "rock") {
+		if (computerSelection === "scissors") {
+			roundForHuman();
+		} else if (computerSelection === "paper") {
+			roundForComputer();
+		}
+	}
+
+	if (humanSelection === "paper") {
+		if (computerSelection === "rock") {
+			roundForHuman();
+		} else if (computerSelection === "scissors") {
+			roundForComputer();
+		}
+	}
+
+	if (humanSelection === "scissors") {
+		if (computerSelection === "paper") {
+			roundForHuman();
+		} else if (computerSelection === "rock") {
+			roundForComputer();
+		}
+	}
+
+	if (humanScore === 5) {
+		humanScore = 0;
+		computerScore = 0;
+		result.textContent = "Congratulations, you won!";
+	} else if (computerScore === 5) {
+		humanScore = 0;
+		computerScore = 0;
+		result.textContent = "Unfortunatelly, computer won :(";
+	}
+}
+
+function roundForComputer() {
+	computerScore++;
+	result.textContent =
+		"You lost this round!" + currentScore(humanScore, computerScore);
+}
+
+function roundForHuman() {
+	humanScore++;
+	result.textContent =
+		"You won this round!" + currentScore(humanScore, computerScore);
+}
 
 function getComputerChoice() {
 	let generateVariable = Math.random();
@@ -21,17 +83,22 @@ function getComputerChoice() {
 	}
 }
 
-function getHumanChoice() {
-	let result = prompt(
-		"Yo dog - what is your choice? Rock, paper or scissors?"
-	).toLowerCase;
-
-	const resultCheck =
-		result === "rock" || result === "paper" || result === "scissors";
-
-	if (!resultCheck) {
-		return console.log("Wrong input!");
-	}
-
-	return result;
+function currentScore(humanScore, computerScore) {
+	return " Human: " + humanScore + " Computer: " + computerScore;
 }
+
+// function getHumanChoice() {
+// 	let result = prompt("Yo dog - what is your choice? Rock, paper or scissors?");
+// 	result = result.toLocaleLowerCase();
+// 	console.log(result);
+
+// 	const resultCheck =
+// 		result === "rock" || result === "paper" || result === "scissors";
+
+// 	if (!resultCheck) {
+// 		console.log("Wrong input! Try again");
+// 		getHumanChoice();
+// 	}
+
+// 	return result;
+// }
